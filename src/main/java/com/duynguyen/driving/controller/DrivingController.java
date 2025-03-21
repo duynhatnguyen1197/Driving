7e941e09198fa70757779bd2614fe0000a323955
duynhatnguyen1197 <ng.h.nhat.duy@gmail.com>
2025-03-22 00:50:39 +0700

    22 Mar work

--- src/main/java/com/duynguyen/driving/controller/DrivingController.java
+++ src/main/java/com/duynguyen/driving/controller/DrivingController.java
@@ -1,13 +1,15 @@
 package com.duynguyen.driving.controller;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+@Controller
 public class DrivingController {
+
     @GetMapping("/")
     public String homePage() {
-        return "index"; // Returns index.html from src/main/resources/templates
+        return "index";
     }
 }
